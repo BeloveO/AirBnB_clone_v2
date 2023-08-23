19e636ecc800f400e31bb5cd87d54b767d9d7160
Belove <beloveolusola@gmail.com>
2023-08-23 19:28:30 +0100

    New ngine

--- models/place.py
+++ models/place.py
@@ -11,10 +11,10 @@ from models.amenity import Amenity
 
 place_amenity = Table("place_amenity", Base.metadata,
                     Column("place_id", String(60),
-                           ForeignKey("places_id"),
+                           ForeignKey("places.id"),
                            primary_key=True, nullable=False),
                     Column("amenity_id", String(60),
-                           ForeignKey("amenities_id"),
+                           ForeignKey("amenities.id"),
                            primary_key=True, nullable=False))
 
 
