a9875c49cd99d2ee1b918f7a78c1a0120280ff6f
Belove <beloveolusola@gmail.com>
2023-09-08 04:12:30 +0100

    Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # Bash script that sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-service nginx start
 mkdir -p /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
@@ -15,10 +14,7 @@ printf "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown ubuntu:ubuntu /data/
-loc_header="location \/hbnb\_static\/ {"
-loc_content="alias \/data\/web\_static\/current\/;"
-new_location="\n\t$loc_header\n\t\t$loc_content\n\t}\n"
-sed -i "37s/$/$new_location/" /etc/nginx/sites-available/default
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a location  /hbnb_static { alias /data/web_static/current; autoindex off; }' /etc/nginx/sites-available/default
 service nginx restart
