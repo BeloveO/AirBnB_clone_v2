6e72badd317ee300630116489f0557dd6c63be9b
Belove <beloveolusola@gmail.com>
2023-09-08 04:42:21 +0100

    Archive to web server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+"""
+Fabric script basd on above that distributes an archive to your web servers
+"""
+import os
+from datetime import datetime
+from fabric.api import *
+import shlex
+
+
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+    else:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
