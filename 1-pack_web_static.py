81f901a5f3f412b28f05bd7ac643ddc776e66ab9
Belove <beloveolusola@gmail.com>
2023-09-11 03:20:19 +0100

    Error fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,16 +10,13 @@ from fabric.api import local
 
 
 def do_pack():
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
         return None
-    return file
