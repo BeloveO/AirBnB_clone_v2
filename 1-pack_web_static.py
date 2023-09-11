92c06ccacdb4dafb7387fe7a4e1fbcb1e60d6dc0
Belove <beloveolusola@gmail.com>
2023-09-11 03:25:54 +0100

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,12 +11,9 @@ from fabric.api import local
 
 def do_pack():
     try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
+        d = datetime.now()
+        now = d.strftime('%Y%m%d%H%M%S')
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+    except FileNotFoundError:
         return None
