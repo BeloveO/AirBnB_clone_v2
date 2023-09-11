d1a8147425b3bdcfee37461c14fa6dc4eb68bfac
Belove <beloveolusola@gmail.com>
2023-09-11 02:26:45 +0100

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,25 +13,39 @@ import shlex
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
-    else:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
+    archive_list = archive_path.split("/")
+    archive_filename = archive_list[-1]
+    filename_list = archive_filename.split(".")
+    filename_noext = filename_list[0]
+    result = put(archive_path, "/tmp/")
+    if result.failed:
+        return False
+    result = run('mkdir -p /data/web_static/releases/{:}/'.format(
+        filename_noext))
+    if result.failed:
+        return False
+    result = run('tar -xzf /tmp/{:} -C /data/web_static/releases/{:}/'.format(
+        archive_filename, filename_noext))
+    if result.failed:
+        return False
+    result = run('rm /tmp/{:}'.format(archive_filename))
+    if result.failed:
+        return False
+    result = run('mv /data/web_static/releases/{:}/web_static/* \
+                 /data/web_static/releases/{:}/'.format(
+                     filename_noext, filename_noext))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/releases/{:}/web_static".format(
+        filename_noext))
+    if result.failed:
+        return False
+    result = run('rm -rf /data/web_static/current')
+    if result.failed:
+        return False
+    result = run(
+        'ln -s /data/web_static/releases/{:}/ /data/web_static/current'.format(
+            filename_noext))
+    if result.failed:
+        return False
+    return True
