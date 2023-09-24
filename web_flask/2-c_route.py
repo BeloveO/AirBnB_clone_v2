734730a52fbd2370003c3eb3b2bffc96f3b225cf
Belove <beloveolusola@gmail.com>
2023-09-24 05:03:53 +0100

    Underscore with space

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,5 +23,13 @@ def hbnb():
     return "HBNB"
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Replacing _ with space
+    """
+    return "C {}".format(text).replace('_', ' ')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
